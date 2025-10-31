90012dc6d19df5151ce5ce091da0d01e0231130c
Sivatejach <chsteja2003@gmail.com>
2025-10-31 17:40:44 +0530

    Test.js added

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,16 @@
+const  accountId = 123456;
+let accountEmail = "chst@example.com"; 
+var accountPassword = "12345";
+accountCity = "New York"; // Implicit global variable (not recommended)
+
+
+
+accountEmail="teja@example.com"
+accountPassword="54321"
+accountCity="Los Angeles"
+/*
+Prefer not to use var beacause it has function scope and can lead to unexpected behaviors.
+Use let and const for block scope variables.
+
+*/
+console.table({ accountEmail, accountCity, accountId, accountPassword }); 
