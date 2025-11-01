597a0e8c7a6bad8150738ece3de3e6e970fadfff
Sivatejach <chsteja2003@gmail.com>
2025-11-01 13:04:07 +0530

    Datatype Operations added

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -75,3 +75,64 @@
 // "abc", " ", [], {} -> true
 //Anything which has some value other than above false values will be treated as true in javascript
 //Used in conditional statements and loops
+
+
+
+
+
+
+
+
+//************************************************Operations*************************************************************** *//
+
+// let value = 3
+// let negValue = -value
+// console.log(value) // 3
+// console.log(negValue) // -3
+// console.log(-negValue) // 3
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2);
+// console.log(2/2);
+
+// let str1= "Hello"
+// let str2= "Chst"
+// let str3= str1 + " " + str2
+// console.log(str3)
+
+
+// console.log("1" + 2) // "12"
+// console.log(1 + "2") // "12"
+// console.log("1" + "2") // "12"
+// console.log(1 + 2) // 3
+// console.log("1"+ 2 + 2) // "122"
+// console.log(1 + 2 + "2") // "32"
+// console.log("1"+ (2 + 2)) // "14"
+// console.log(1 + (2 + "2")) // "32"
+
+//In the presence of a string in addition operation everything will be converted to string and concatenated
+
+
+// console.log(+true) // 1
+// console.log(+false) // 0
+// console.log(+"") // 0
+// console.log(1+2+"33") // "3333"
+// console.log(+"33") // 33
+// console.log("33"+ 0) // "330"
+// console.log(+"C") // NaN
+// console.log(+"33abc") // NaN
+// console.log(+null) // 0
+// console.log(+undefined) // NaN
+// console.log(NaN + 3) // NaN
+// console.log(3 + undefined) // NaN
+
+//difference between undefined and null in arithmetic operations
+//undefined when used in arithmetic operations will return NaN
+//null when used in arithmetic operations will be treated as 0
+
+//WHAT IS NaN?
+//NaN is a special numeric value which indicates not a number
+//NaN is contagious in javascript
+//If any operand in an arithmetic operation is NaN the result will be Na
