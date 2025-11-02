268a48f318c063bd55c8cf9744e8313d093abe60
Sivatejach <chsteja2003@gmail.com>
2025-11-02 11:40:34 +0530

    Added nums.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -14,3 +14,11 @@ console.log(num.toPrecision(3));
 
 const numberval= 1000000
 console.log(numberval.toLocaleString("en-IN"));
+
+
+
+//+++++++++++++++++++++++++++++++++++++++MMaths++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math)
