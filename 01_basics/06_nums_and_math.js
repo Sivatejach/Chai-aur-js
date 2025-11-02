5d70db8d18afc4612309038f72523825f3c76608
Sivatejach <chsteja2003@gmail.com>
2025-11-02 11:32:48 +0530

    added nums,js

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,12 @@
+const score=400;
+console.log("Your score is: " + score);
+
+const balance =new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+const num= 23.86444
+console.log(num.toPrecision(3));
