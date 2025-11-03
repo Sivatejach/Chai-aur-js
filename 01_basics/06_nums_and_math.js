f64efd92ca48cf85df909de735962603743f8646
Sivatejach <chsteja2003@gmail.com>
2025-11-03 10:55:33 +0530

    Math added

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -19,6 +19,20 @@ console.log(numberval.toLocaleString("en-IN"));
 
 //+++++++++++++++++++++++++++++++++++++++MMaths++++++++++++++++++++++++++++++++++++++++++
 
+
+//Math is by default library in js and mostlu used for mathematical operations
 console.log(Math.PI);
 console.log(Math.E);
 console.log(Math)
+
+
+
+
+console.log(Math.random()); //generates random number between 0 to 1
+
+console.log(Math.round(4.6));//rounds to nearest integer
+console.log(Math.round(4.4));//rounds to nearest integer
+console.log(Math.floor(4.9));
+console.log(Math.floor(0.1)+1); //Outputs =0 because it always rounds down to nearest integer
+console.log(Math.ceil(4.1)); //always rounds up to greater integer
+console.log(Math.trunc(4.9)); //removes decimal part without rounding
